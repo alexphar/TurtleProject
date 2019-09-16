abdab33f499632fa5d386b313ae31a413a325eb0
Alex Rapatti <t7raal00@students.oamk.fi>
2019-09-16 13:32:56 +0300

    First commit

--- Assets/DBClass.cs
+++ Assets/DBClass.cs
@@ -5,17 +5,16 @@ using UnityEngine.UI;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
-
-
 public class DBClass : MonoBehaviour
     {
 
-        private MySqlConnection connection;
+        private MySqlConnection connection; //alustetaan yhteyteen tarvittavat tiedot
         private string server;
         private string database;
         private string uid;
         private string password;
-		public string lampoTesti {get; set;}
+		
+		public string lampoTesti {get; set;} //alustetaan databasin arvot jotta niitä voidaan lukea ja ottaa käyttöön
 		public string kulmax {get; set;}
 		public string kulmay {get; set;}
 		public string kulmaz {get; set;}
@@ -24,7 +23,7 @@ public class DBClass : MonoBehaviour
 
         public DBClass() //konstruktori
         {
-            Initialize();
+            Initialize(); //kutsuu alustajaa
         }
 
         private void Initialize() //alustetaan connectionstringin arvot database yhteyden tiedoilla
@@ -33,11 +32,13 @@ public class DBClass : MonoBehaviour
             database = "opisk_t7hani00";
             uid = "t7hani00";
             password = "NEwttgtub7h3FHYq";
-            string connectionString;
-            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            
+			string connectionString; //luodaan connectionstring yhteyttä varten. tarvitaan databasen omistajan tiedot
+            
+			connectionString = "SERVER=" + server + ";" + "DATABASE=" +
             database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
 
-            connection = new MySqlConnection(connectionString);
+            connection = new MySqlConnection(connectionString); //luodaan tästä connectionstringistä olio jotta yhteys voidaan avata ja sulkea
         }
 
         public bool OpenConnection() //avataan yhteys
@@ -49,7 +50,7 @@ public class DBClass : MonoBehaviour
             }
             catch (MySqlException ex)
             {
-                Debug.LogError("MySQL Error: " + ex.ToString());
+                Debug.LogError("MySQL Error: " + ex.ToString()); //tuodaan error viesti jos yhteyden luomisessa on ongelmia
             }
                 return false;
             }
@@ -70,39 +71,35 @@ public class DBClass : MonoBehaviour
             }
         }
 
-		public void readData() //haetaan dataa
+		public void readData() //haetaan dataa ja luetaan
 		{			
-			string query = "SELECT * FROM sensori_data WHERE data_id = 1"; //valitaan kaikki sensori_data taulusta
+			string query = "SELECT * FROM sensori_data WHERE data_id = 1"; //valitaan kaikki arvot sensori_data taulusta
 
-                var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
-                var reader = cmd.ExecuteReader();
+                var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection); //luodaan olio joka tietää connection stringin ja queryn
+                var reader = cmd.ExecuteReader(); //reader joka osaa lukea arvoja tietokannasta
 
                 while (reader.Read())
                 {
-                    var lampo = reader["lampotila"];
-					lampoTesti = lampo.ToString();	//luetaan lampotila taulusta sensori_data
+                    var lampo = reader["lampotila"]; //asetetaan lampotila muuttujaksi taulusta sensori_data
+					lampoTesti = lampo.ToString();	//lampotesti on string lampotilasta. voidaan hyödyntää muun muuassa tekstien kautta
                     
-					var kulma1 = reader["kulma_x"]; //luetaan kulma_x taulusta sensori_data
+					var kulma1 = reader["kulma_x"]; //asetetaan kulma_x muuttujaksi taulusta sensori_data
 					kulmax = kulma1.ToString();
 					
-                    var kulma2 = reader["kulma_y"];
+                    var kulma2 = reader["kulma_y"]; //asetetaan kulma_y muuttujaksi taulusta sensori_data
 					kulmay = kulma2.ToString();
 					
-                    var kulma3 = reader["kulma_z"];
+                    var kulma3 = reader["kulma_z"]; //asetetaan kulma_z muuttujaksi taulusta sensori_data
 					kulmaz = kulma3.ToString();
 					
-					var nalka = reader["nalka"];
+					var nalka = reader["nalka"]; //asetetaan nalka muuttujaksi taulusta sensori_data
 					nalkaArvo = nalka.ToString();
 
-                    //label1.Text = "x: " + kulma1; //kulma_x:n arvo
-                    //label2.Text = "y: " + kulma2; //y
-                    //label3.Text = "z: " + kulma3; //z
-					Debug.Log("lampotila: " + lampo);//lampötilan arvo
+					Debug.Log("lampotila: " + lampo);//luetaan arvot debuggina. tässä lämpötilan lukua 
 					Debug.Log("kulmax: " + kulma1);
 					Debug.Log("kulmay: " + kulma2);
 					Debug.Log("kulmaz: " + kulma3);
 					Debug.Log("nalka: " + nalka);
-                    //counter++; //timerin counter testaamista varten
 
                 }
 		}
@@ -113,14 +110,14 @@ public class DBClass : MonoBehaviour
 			
 			if(int.Parse(nalkaArvo) < 500) //estetään nälkäarvoa menemästä miinuksen puolelle
 			{
-				query = "UPDATE sensori_data SET nalka = 0 WHERE data_id = 1";
+				query = "UPDATE sensori_data SET nalka = 0 WHERE data_id = 1"; //asetetaan nalka nollaksi tässä tilanteessa
 			}
 			else
 			{
-				query = "UPDATE sensori_data SET nalka=nalka - 500 WHERE data_id = 1"; //vähennettävä määrä nälkäarvosta
+				query = "UPDATE sensori_data SET nalka=nalka - 500 WHERE data_id = 1"; //vähennettävä haluttu määrä nälkäarvosta
 			}
 				
-			MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
-			cmd.ExecuteNonQuery();
+			MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection); //tarvittava olio databasen yhteydestä ja querystä
+			cmd.ExecuteNonQuery(); //toteutetaan query
 		}
 	}
--- Assets/ui_teksti_script.cs
+++ Assets/ui_teksti_script.cs
@@ -1,71 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; //tarvitaan UI:n tekstiä varten
+using UnityEngine.UI; //tarvitaan UI:n tekstiä ja nappia varten
 using MySql.Data;
-using MySql.Data.MySqlClient; //lisää mysql dll:t ja system data dll assetseihin ja muuta asetuksista project settings -> player ja valitse net 4x
+using MySql.Data.MySqlClient; //tarvittava mysql kirjasto yhteyttä varten. lisää mysql dll:t ja system data dll assetseihin ja muuta asetuksista project settings -> player ja valitse net 4x
 
 public class ui_teksti_script : MonoBehaviour {
 
-
-	public Text testiTeksti; //muista lisätä ui_teksti_scriptiin teksti objecti
+	public Text testiTeksti; //muista lisätä ui_teksti_scriptiin teksti objekti jotta se voidaan ottaa käyttöön
 	public Text teksti2;
 	public Text teksti3;
 	public Text teksti5;
 	public Text rotationText;
 	public Text temperatureText;
+	
 	public Button foodButton;
 
-    AudioSource ree1;
-    AudioSource ree2;
+    AudioSource ree1; //äänite1
+    AudioSource ree2; //äänite2
 
     bool m_Play;
 	bool m_Play2;
 
 	private DBClass testi;
 	
-	
-	
-	// Use this for initialization
+	// alustaja
 	void Start () {
-	Debug.Log("Startissa");
-        //testiTeksti.text = "moi";
-        AudioSource[] audioSources = GetComponents<AudioSource>();
-        ree1 = audioSources[0];
+		
+		Debug.Log("Startissa");
+
+        AudioSource[] audioSources = GetComponents<AudioSource>(); //lisätään audiolista objektin komponentiksi
+        ree1 = audioSources[0]; //lisätään äänitteet audiolistaan
         ree2 = audioSources[1];
-        testi = gameObject.AddComponent<DBClass>(); //lisätään db luokka objectin komponenteista
-        m_Play = false;
+        
+		testi = gameObject.AddComponent<DBClass>(); //lisätään db luokka objektin komponenteista
+        
+		m_Play = false; //asetetaan booleat falseksi jotta ne voidaan ottaa käyttöön vain tietyissä tilanteissa
 		m_Play2 = false;
 		
-		foodButton.onClick.AddListener(clicked); //ruokintanappi
+		foodButton.onClick.AddListener(clicked); //ruokintanappi kuuntelee clicked funktiota, eli klikatessa konnalta vähenee nälkäpisteitä
 	
 	}
 	
-	// Update is called once per frame
+	// päivittää once per frame
 	void Update () {
-	 
-	 //testi = GetComponen<DBClass>();	
-     testi.OpenConnection();
-	 testi.readData(); //asetetaan lämpöarvo muuttujaan
-	 textData(); //asetetaan arvot tekstiobjekteihin
-	 
-	 transform.localEulerAngles = new Vector3(float.Parse(testi.kulmax),0.0f,float.Parse(testi.kulmay)); //asetetaan objektille (kokeiluvaiheessa cube) arvot x y ja z jotta objekti voi liikkua sensoreiden antamilla arvoilla
-	 
-	 wrongRotation(); //valitusteksti kun konna on selällään tai kyljellään
-	 temperature(); //lämpöreaktiot
-	 
-     testi.CloseConnection();
+	 	
+		 testi.OpenConnection(); //avataan yhteys DB luokan funktiolla
+		 testi.readData(); //käytetään funktiota joka asettaa databasin arvot muuttujiksi joita pystytään lukemaan
+		 textData(); //asetetaan arvot teksti objekteihin jotta käyttäjä voi lukea arvoja ilman control panelia
+		 
+		 transform.localEulerAngles = new Vector3(float.Parse(testi.kulmax),0.0f,float.Parse(testi.kulmay)); //asetetaan objektille (kokeiluvaiheessa cube) arvot x z ja y jotta objekti voi liikkua sensoreiden antamilla arvoilla
+		 
+		 wrongRotation(); //käytetään funktiota joka tuo esille valitustekstin kun konna on selällään tai kyljellään
+		 temperature(); //funktio tuo esille konnan lämpöreaktiot
+		 
+		 testi.CloseConnection(); //suljetaan yhteys
 	}
 	
-	void textData () {
+	void textData () { //asetetaan teksti objekteille luettavat arvot
 	 
-	 testiTeksti.text = "lämpötila: " + testi.lampoTesti; //luetaan lämpöarvo tekstin kautta
-	 teksti2.text = "kulma x: " + testi.kulmax;
-	 teksti3.text = "kulma y: " + testi.kulmay;
-	 teksti5.text = "nälkä: " + testi.nalkaArvo;
+		 testiTeksti.text = "lämpötila: " + testi.lampoTesti; //luetaan lämpöarvo tekstin kautta
+		 teksti2.text = "kulma x: " + testi.kulmax; //kulma x
+		 teksti3.text = "kulma y: " + testi.kulmay; //kulma y
+		 teksti5.text = "nälkä: " + testi.nalkaArvo; //nälkälukema
 	}
 	
 	void clicked () { //päivitetään nälkäarvoa napin painalluksella. 500 nälkäpistettä vähenee per klikkaus
+		
 		Debug.Log("Nappia painettu");
 		
 		testi.OpenConnection();
@@ -73,11 +74,11 @@ public class ui_teksti_script : MonoBehaviour {
 		testi.CloseConnection();
 	}
 	
-	void wrongRotation () { //asetetaan arvot kulmille jotta valitusteksti tulee esille kun konna on selällään
+	void wrongRotation () { //asetetaan arvot kulmille jotta valitusteksti tulee esille kun konna on selällään. valitusääni alkaa jo 90 kulman kohdalla
 			 
-	  if (float.Parse(testi.kulmax) < -130 || float.Parse(testi.kulmax) > 130 || float.Parse(testi.kulmay) < -130 || float.Parse(testi.kulmay) > 130) //selällään
+	  if (float.Parse(testi.kulmax) < -130 || float.Parse(testi.kulmax) > 130 || float.Parse(testi.kulmay) < -130 || float.Parse(testi.kulmay) > 130) //jos konna on selällään
 	  {
-		rotationText.enabled = true;
+		rotationText.enabled = true; //otetaan teksti käyttöön
 		rotationText.text = "REEEEEE KÄÄÄNNÄ MUT";
 	  }
 	  
@@ -85,14 +86,14 @@ public class ui_teksti_script : MonoBehaviour {
 	  {
 		rotationText.enabled = true;
 		rotationText.text = "REEEEEEEE";
-            if(m_Play == false)
+            if(m_Play == false) //jos äänite ei ole jo käynnissä, soitetaan äänite 1
             {
                 ree1.Play();
                 m_Play = true;
             }
 	  }
 	  
-	  else
+	  else //muussa tapauksessa otetaan teksti ja äänite pois käytöstä
 	  {
 		 rotationText.enabled = false;
             if(m_Play == true)
@@ -105,9 +106,9 @@ public class ui_teksti_script : MonoBehaviour {
 	
 	void temperature () { //lampötilan vaikutus 
 		
-		if(float.Parse(testi.lampoTesti) < 15 && float.Parse(testi.lampoTesti) >= 10 )
+		if(float.Parse(testi.lampoTesti) < 15 && float.Parse(testi.lampoTesti) >= 10 ) //jos lämpötilä on pienempi kuin 15 mutta suurempi tai yhtäsuuri kuin 10
 		{
-			temperatureText.enabled = true;
+			temperatureText.enabled = true; //teksti käyttöön
 			temperatureText.text = "Onpa kylmä!";
 		}
 		
@@ -116,7 +117,7 @@ public class ui_teksti_script : MonoBehaviour {
 			temperatureText.enabled = true;
 			temperatureText.text = "Väsyttää.. horrostila kutsuu";
 			
-			if(m_Play2 == true)
+			if(m_Play2 == true) //otetaan äänite 2 pois käytöstä
             {
                 ree2.Stop();
                 m_Play2 = false;
@@ -128,7 +129,7 @@ public class ui_teksti_script : MonoBehaviour {
 			temperatureText.enabled = true;
 			temperatureText.text = "Kuollut";
 			
-			if(m_Play2 == false)
+			if(m_Play2 == false) //laitetaan äänite 2 käyttöön kun lämpötila laskee alle nollaan
 			{
 				ree2.Play();
 				m_Play2 = true;
@@ -142,10 +143,10 @@ public class ui_teksti_script : MonoBehaviour {
 		else if(float.Parse(testi.lampoTesti) > 45 && float.Parse(testi.lampoTesti) <= 60)
 		{
 			temperatureText.enabled = true;
-			temperatureText.text = "Voitko viedä minut varjoon?";
+			temperatureText.text = "Voitko viedä mut varjoon?";
 			
-			if(m_Play2 == true)
-            {
+			if(m_Play2 == true) //varmistetaan ettei äänite ole päällä
+            { 
                 ree2.Stop();
                 m_Play2 = false;
             }
@@ -156,7 +157,7 @@ public class ui_teksti_script : MonoBehaviour {
 			temperatureText.enabled = true;
 			temperatureText.text = "Kuollut";
 			
-			if(m_Play2 == false)
+			if(m_Play2 == false) //käytetään äänite 2 jos lämpötila nousee yli 60 astetta
             {
                 ree2.Play();
                 m_Play2 = true;
@@ -164,7 +165,7 @@ public class ui_teksti_script : MonoBehaviour {
 		}
 		else
 		{
-			temperatureText.enabled = false;
+			temperatureText.enabled = false; //muussa tapauksessa otetaan teksti ja äänite pois käytöstä
 			
 			 if(m_Play2 == true)
             {
